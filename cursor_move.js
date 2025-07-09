de79f02242daabbb4d0e4c78d07f2d3b96ba46c6
Shruti <shruticg2013@gmail.com>
2025-07-09 12:48:43 +0530

    Basics

--- cursor_move.js
+++ cursor_move.js
@@ -1,1 +1,21 @@
-import * as THREE form 'three';
+import * as THREE from 'three';
+
+const scene = new THREE.scene();
+const camera = new THREE.Perspectivecamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const reneder = new THREE.WebGlRenderer();
+
+// Geometry, mesh and material 
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+
+
+scene.add(cube);
+camera.position.z = 5;
+scene.add(camera);
+reneder.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(reneder.domElement);
+
+
+
