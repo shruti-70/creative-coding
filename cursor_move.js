ed1bd35644ff81d3de967739b81c59cc1051ea01
Shruti <shruticg2013@gmail.com>
2025-07-14 15:49:44 +0530

    anime.js exploration

--- cursor_move.js
+++ cursor_move.js
@@ -1,21 +1,25 @@
 import * as THREE from 'three';
 
-const scene = new THREE.scene();
-const camera = new THREE.Perspectivecamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const reneder = new THREE.WebGlRenderer();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-// Geometry, mesh and material 
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-
-scene.add(cube);
 camera.position.z = 5;
-scene.add(camera);
-reneder.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(reneder.domElement);
 
+function animate() {
 
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 
+  renderer.render( scene, camera );
+
+}
