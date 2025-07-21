ac5ab864eb447c2a8b5996503ec82e55ba15d45d
Shruti <shruticg2013@gmail.com>
2025-07-21 18:43:15 +0530

    basics

--- scene1.js
+++ scene1.js
@@ -10,8 +10,21 @@ let mesh = new THREE.Mesh(
     new THREE.MeshBasicMaterial({ color: "green"})
 );
 
+// mesh.position.z = -3;
+// rotation, poistion and scale
+// mesh.scale.z = 2;
+// mesh.rotation.z = 2;
+// mesh.rotation.x = Math.PI; // 180- degrees rotatation back to same position
 scene.add(mesh);
+
 
 let renderer = new THREE.WebGLRenderer({canvas: document.querySelector("#cube")});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
+
+function animate(){
+    window.requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    mesh.rotation.y+= 0.01;
+}
+animate();
